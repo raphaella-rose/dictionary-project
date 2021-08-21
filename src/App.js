f0741316223cd5e91af180b77b552470724f507a
raphaella-rose <raphaella.rose1@gmail.com>
2021-08-21 11:38:08 +0100

    created form and search function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="text-center">
+      Dictionary
+    </h1> 
+    </header>
+    <main>
+    <Dictionary />
+    </main>
+    <footer>
+      <div className="text-center">
+      Coded by Raphaella D'Alessandro
+    </div>
+    </footer>
     </div>
   );
 }
